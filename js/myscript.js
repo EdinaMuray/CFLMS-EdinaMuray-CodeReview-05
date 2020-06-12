f0d369c7947aeef1144edd8eac20b044dad90d39
EdinaMuray <ing.edina.muray@gmail.com>
2020-06-12 21:08:50 +0200

    json

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,6 @@
 var myMovies = JSON.parse(movie);
 
-		for (let i = 0; i < myMovies.length; i++) {
+for (let i = 0; i < myMovies.length; i++) {
 
-			document.getElementById('result').innerHTML += "<ul><li>" + myMovies[i].titel + "</li>" + "<li> Description: " + myMovies[i].description + "</li>" + "<li class='pic'> <img src='" + myMovies[i].cover + "'></li></ul>";
-		}
+	document.getElementById('result').innerHTML += "<ul><li>" + myMovies[i].titel + "</li>" + "<li> Description: " + myMovies[i].description + "</li>" + "<li class='pic'> <img src='" + myMovies[i].cover + "'></li></ul>";
+}
