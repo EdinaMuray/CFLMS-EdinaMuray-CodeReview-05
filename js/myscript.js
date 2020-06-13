40872e4a10c0c983775171b3ed6383f5319e191f
EdinaMuray <ing.edina.muray@gmail.com>
2020-06-13 15:29:28 +0200

    jquery

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,27 @@
 var myMovies = JSON.parse(movie);
+console.log(myMovies)
 
-for (let i = 0; i < myMovies.length; i++) {
+$(document).ready(function(){
 
-	document.getElementById('result').innerHTML += "<ul><li>" + myMovies[i].titel + "</li>" + "<li> Description: " + myMovies[i].description + "</li>" + "<li class='pic'> <img src='" + myMovies[i].cover + "'></li></ul>";
-}
+	var bigbox = $("<div></div>").addClass("bigbox")
+	$("main").prepend(bigbox)
+
+	for (i = 0; i < myMovies.length; i++) {
+
+		smallbox = $("<div></div>").addClass("smallbox")
+		bigbox.append(smallbox)
+
+		halfbox = $("<div></div>").addClass("halfbox")
+		smallbox.append(halfbox)
+
+		var cover = $("<img>")
+		var titel = $("<h1>" + myMovies[i].titel + "</h1>")
+		var description = $("<p>" + myMovies[i].description + "</p>")
+		var button = $("<button class ='like_button'>" + myMovies[i].button + "</button>" )
+
+		smallbox.append(cover).append(titel).append(description).append(button)
+		halfbox.append(cover)
+		cover.attr("src", myMovies[i].cover)
+
+	}
+});
